2c360d87929c2dc4fa53fb5e9e2e899f77c4953e
Anders Karlsen <anderska@nmbu.no>
2019-09-14 12:34:27 +0200

    Created for-loop function according to exercise

--- src/anders_karlsen_ex/ex01/comp_to_loop.py
+++ src/anders_karlsen_ex/ex01/comp_to_loop.py
@@ -2,3 +2,12 @@ def squares_by_comp(n):
     return [k**2 for k in range(n) if k % 3 == 1]
 
 
+def squares_by_loop(n):
+    squares = []
+    for k in range(n):
+        if k % 3 == 1:
+            squares.append(k**2)
+    return squares
+
+
+
