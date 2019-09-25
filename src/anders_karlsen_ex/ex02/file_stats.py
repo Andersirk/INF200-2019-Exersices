c19d9d488741d22ff09ed00a7a0da3413750bd7e
Anders Karlsen <anderska@nmbu.no>
2019-09-25 09:10:39 +0200

    Created the code for char_counts ex02 Task A

--- src/anders_karlsen_ex/ex02/file_stats.py
+++ src/anders_karlsen_ex/ex02/file_stats.py
@@ -1,5 +1,25 @@
+from collections import Counter
+
+
 def char_counts(textfilename):
-    pass
+    """ opens the file with the given filename
+     using encoding utf-8 and reads the entire file content into a single string.
+    then counts how often each character code (0–255) occurs in the string
+    Returns
+    -------
+    return the result as a list, where result[i] gives the number
+    of occurances of charactor code i.
+    """
+    with open(textfilename, encoding="utf-8") as file:
+        list_of_lines = file.readlines()
+        list_of_lines_no_newline = [line[:-2] for line in list_of_lines]
+        string = "".join(list_of_lines_no_newline)
+        freq_of_characters = Counter(string)
+        list_character_freq = [0 for _ in range(256)]
+        for key, value in freq_of_characters.items():
+            if ord(key) < 256:
+                list_character_freq[ord(key)] = value
+    return list_character_freq
 
 
 if __name__ == '__main__':
