5bfce379350acd1338ad83ce254c39139c347b16
Anders Karlsen <anderska@nmbu.no>
2019-10-02 13:30:46 +0200

    Created file for median test and added code from exercise

---
+++ src/anders_karlsen_ex/ex03_project/test_median.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Karlsen'
+__email__ = 'anderska@nmbu.no'
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+
+
