becff3c8b8d3837eeeca7e84fa4a7f521ed76038
Anders Karlsen <anderska@nmbu.no>
2019-10-02 15:07:35 +0200

    Created template for median tests in Task B

--- src/anders_karlsen_ex/ex03_project/test_median.py
+++ src/anders_karlsen_ex/ex03_project/test_median.py
@@ -18,3 +18,56 @@ def median(data):
         else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
 
 
+def test_one_element():
+    """
+    Tests that the median function
+    returns correct value for one-element list
+    """
+    pass
+
+
+def test_odd_number_elements():
+    """Tests that the median function returns correct value for
+    a list with odd number of elements"""
+    pass
+
+def test_even_number_elements():
+    """Tests that the median function returns correct value for
+    a list with even number of elements"""
+    pass
+
+
+def test_ordered_elements():
+    """Tests that the median function returns correct value for
+    a list with ordered elements"""
+    pass
+
+
+def test_reverse_ordered_elements():
+    """Tests that the median function returns correct value for
+    a list with reverse ordered elements"""
+    pass
+
+
+def test_unordered_elements():
+    """Tests that the median function returns correct value for
+    a list with unordered elements"""
+    pass
+
+
+def test_error():
+    """Tests that the median function raises valuerror
+    for empty list"""
+    pass
+
+
+def test_original_unchanged():
+    """test that ensures that the median function
+     leaves the original data unchanged"""
+    pass
+
+
+def test_tuples_and_lists():
+    """test that ensures that the median function
+     works for tuples as well as lists"""
+    pass
