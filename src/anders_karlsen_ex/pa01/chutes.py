db5d55a460d9c8dc3ccd8970f00adb0588dabb12
Kaare Johnsen <kajohnse@nmbu.no>
2019-10-03 14:33:57 +0200

    Edited some errors

--- src/anders_karlsen_ex/pa01/chutes.py
+++ src/anders_karlsen_ex/pa01/chutes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__author__ = "Kåre Johnsen, Anders Karlsen"
-__email__ = "kajohnse@nmbu.no, anderska@nmbu.no"
+__author__ = "Kåre Johnsen", "Anders Karlsen"
+__email__ = "kajohnse@nmbu.no", "anderska@nmbu.no"
 
 # Jeg endret på koden
