4215e4ab1e54a4c2122353c8ed7c7df0d4376c45
Anders Karlsen <anderska@nmbu.no>
2019-10-26 13:33:58 +0200

    Created file for Task B ex05

---
+++ src/anders_karlsen_ex/ex05/bounded_sim.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anders Karlsen'
+__email__ = 'anderska@nmbu.no'
+
+from .walker_sim import Walker, Simulation
+
+
+class BoundedWalker(Walker):
+    pass
+
+
+class BoundedSimulation:
+    pass
