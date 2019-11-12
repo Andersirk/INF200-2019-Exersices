6ff8f710a0b345e24d831f02a183fdc3d90fcc41
Anders Karlsen <anderska@nmbu.no>
2019-11-12 13:25:19 +0100

    Created classes for player and resiliant player

--- src/anders_karlsen_ex/pa02/snakes_simulation.py
+++ src/anders_karlsen_ex/pa02/snakes_simulation.py
@@ -3,12 +3,14 @@
 __author__ = "Anders Karlsen", "Kåre Johnsen"
 __email__ = "anderska@nmbu.no", "kajohnse@nmbu.no"
 
+import random
+
 
 class Board:
     def __init__(self, *args, **kwargs):
-        self.snakes_and_ladders = {1:40, 8:10, 36:52, 43:62, 49:79, 65:82, 68:85,
-                              25:5, 33:3, 42:30, 56:37, 64:27, 74:12, 87:70}
-        self.arguments = args
+        self.ladders = {1:40, 8:10, 36:52, 43:62, 49:79, 65:82, 68:85}
+        self.snakes = {25:5, 33:3, 42:30, 56:37, 64:27, 74:12, 87:70}
+        self.extra_move = False
         self.goal = 90
 
     def goal_reached(self, position):
@@ -17,22 +19,39 @@ class Board:
         return False
 
     def position_adjustment(self, position):
-        if position in self.snakes_and_ladders.keys():
-            return self.snakes_and_ladders[position] - position
+        if position in self.snakes.keys():
+            self.extra_move = True
+            return self.snakes[position] - position
+        elif position in self.ladders.keys():
+            return self.ladders[position] - position
         else:
             return 0
-
 
 
 class Player:
     def __init__(self, board):
         self.board = board
+        self.position = 0
+        self.number_of_throws = 0
 
-    def move():
-        pass
+    def move(self):
+        self.position += random.randint(1, 6)
+        self.position += self.board.position_adjustment(self.position)
+        self.number_of_throws += 1
+
 
 class ResilientPlayer(Player):
-    pass
+    def __init__(self, board, extra_steps=1):
+        super().__init__(board)
+        self.extra_step = extra_steps
+
+    def move(self):
+        self.position += random.randint(1, 6)
+        if self.board.extra_move:
+            self.position += self.extra_steps
+        self.position += self.board.position_adjustment(self.position)
+        self.number_of_throws += 1
+
 
 class LazyPlayer(Player):
     pass
