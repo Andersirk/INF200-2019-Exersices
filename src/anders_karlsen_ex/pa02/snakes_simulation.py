7fb331fd029a6115a9a67be41d633df44d9724d6
Anders Karlsen <anderska@nmbu.no>
2019-11-12 14:03:51 +0100

    did some very minor changes

--- src/anders_karlsen_ex/pa02/snakes_simulation.py
+++ src/anders_karlsen_ex/pa02/snakes_simulation.py
@@ -10,7 +10,8 @@ class Board:
     def __init__(self, *args, **kwargs):
         self.ladders = {1:40, 8:10, 36:52, 43:62, 49:79, 65:82, 68:85}
         self.snakes = {25:5, 33:3, 42:30, 56:37, 64:27, 74:12, 87:70}
-        self.extra_move = False
+        self.resilient_move = False
+        self.lazy_move = False
         self.goal = 90
 
     def goal_reached(self, position):
@@ -20,9 +21,10 @@ class Board:
 
     def position_adjustment(self, position):
         if position in self.snakes.keys():
-            self.extra_move = True
+            self.resilient_move = True
             return self.snakes[position] - position
         elif position in self.ladders.keys():
+            self.lazy_move = True
             return self.ladders[position] - position
         else:
             return 0
@@ -47,19 +49,57 @@ class ResilientPlayer(Player):
 
     def move(self):
         self.position += random.randint(1, 6)
-        if self.board.extra_move:
+        if self.board.resilient_move:
             self.position += self.extra_steps
         self.position += self.board.position_adjustment(self.position)
         self.number_of_throws += 1
 
 
 class LazyPlayer(Player):
-    pass
+    def __init__(self, board, dropped_steps=1):
+        super().__init__(board)
+        self.dropped_steps = dropped_steps
+
+    def move(self):
+        throw = random.randint(1, 6)
+        if self.board.lazy_move:
+            if throw - self.dropped_steps <= 0:
+                return
+            self.position += throw - self.dropped_steps
+        self.position += self.board.position_adjustment(self.position)
+        self.number_of_throws += 1
 
 class Simulation:
-    pass
+    def __init__(self, seed, randomize_players, list_of_players):
+        self.seed = seed
+        if randomize_players:
+            random.shuffle(list_of_players)
+        self.list_of_players = list_of_players
+        self.list_of_initiated_players = [player(Board) for player in list_of_players]
 
+    def single_game(self):
+        while True:
+            for player in self.list_of_initiated_players:
+                player.move()
+                if player.board.goal_reached(player.position):
+                    return (player.number_of_throws, type(player).__name__)
+
+
+    def run_simulation(self):
+        pass
+
+    def get_results(self):
+        pass
+
+    def winners_per_type(self):
+        pass
+
+    def durations_per_type(self):
+        pass
+
+    def players_per_type(self):
+        pass
 
 if __name__ == "__main__":
-    pork = Board()
-    pork.position_adjustment(1)
+    testert = Simulation(69, True, [Player, ResilientPlayer])
+    testert.single_game()
